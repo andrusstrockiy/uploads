8c84e1a0bf2120dac78916c7f09d4b4e0f3b5400
Andruss Trockiy <trockiy@gmail.com>
2018-01-11 12:32:50 +0300

    deploy commit

---
+++ emails.py
@@ -0,0 +1,10 @@
+from flask_mail import Mail, Message
+
+
+# from app import mail
+
+def send_email(subject, sender, recipients, text_body, ):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    # msg.html = html_body
+    return msg
--- uploader.py
+++ uploader.py
@@ -3,6 +3,10 @@ from flask import Flask, request, redirect, url_for, flash, render_template
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 from passwordhelper import PasswordHelper
+from flask_mail import Mail, Message
+
+
+from emails import send_email
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -13,6 +17,8 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.secret_key = "sISH72eQKgv9LpFcfnKdjR1"
+mail = Mail(app)
+
 
 
 def allowed_file(filename):
@@ -25,8 +31,8 @@ def upload_file():
     filesdata = []
     if request.method == 'POST':
         for file in request.files.getlist('files'):
-            print(file.filename,file.content_type,file.stream,file.name)
-            print(request.files)
+            # print(request.files)
+            # print(file.filename, file.content_type, file.stream, file.name)
             # check if the post request has the file part
             if not file.name == 'files':
                 flash('No file part')
@@ -43,26 +49,62 @@ def upload_file():
                 hashed = PH.get_hash(filename + salt)[:16]
                 file_dirhash = os.path.join(app.config['UPLOAD_FOLDER'], hashed)
                 os.makedirs(file_dirhash)
-                fdata ={
-                    'filehash':file_dirhash,
-                    'filename':filename
+                fdata = {
+                    'filehash': file_dirhash,
+                    'filename': filename
                 }
                 filesdata.append(fdata)
                 file.save(os.path.join(file_dirhash, filename))
                 flash("files are uploaded")
         # return 'Upload complete.'
-            #
+        #
         return render_template('preview.html',
-                                   fdata=filesdata,
-                                   purl=PROJECT_URL,
+                               fdata=filesdata,
+                               purl=PROJECT_URL,
                                )
     return render_template('home.html')
 
+
 @app.route('/uploads/<file_dir>/<filename>')
 def uploaded_file(filename, file_dir):
-    file_dirhash = os.path.join(app.config['UPLOAD_FOLDER'],file_dir)
+    file_dirhash = os.path.join(app.config['UPLOAD_FOLDER'], file_dir)
     print(file_dirhash)
     return send_from_directory(file_dirhash, filename)
+
+
+@app.route('/sentemail', methods=["POST"])
+def sendemail():
+    app.config.update(
+        DEBUG=True,
+        # EMAIL SETTINGS
+        MAIL_SERVER='mx.lanbilling.ru',
+        MAIL_PORT=25,
+        MAIL_USE_TLS=True,
+        MAIL_USERNAME='tkachenko@lanbilling.ru',
+        MAIL_PASSWORD='XsKtu8thaW6'
+    )
+    mail = Mail(app)
+    print(request.form.get('filesurl'))
+    try:
+        email = send_email(subject='Files',
+                       sender='support@lanbilling.ru',
+                        recipients=[request.form.get('email')],
+                        text_body=render_template('template_email',files=files,user=))
+        mail.send(email)
+        flash('Thanks your email is sent')
+    except Exception as e:
+        return(str(e))
+    # if request.method == 'POST' and request.form['email']:
+    #     send_email(subject='files',
+    #                sender='files@lanbilling.ru',
+    #                recipients=request.form['email'],
+    #                text_body='')
+
+
+#
+# send_email(subject='files',
+#            sender='files@lanbilling.ru',
+#            recipients=request.form.)
 
 
 if __name__ == '__main__':
